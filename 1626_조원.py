ea32579ccee3e224adf136fbcc36964e456ce032
EthanWonCho <realethanwoncho@gmail.com>
2025-12-04 01:01:54 +0900

    idk

--- 1626_조원.py
+++ 1626_조원.py
@@ -398,7 +398,7 @@ def main():
         def render_treble(self):
             treble_item = QGraphicsSvgItem()
             treble_item.setSharedRenderer(self.renderer_treble)
-            treble_item.setScale(0.085)
+            treble_item.setScale(0.08)
             treble_item.setPos(50, 93)
             treble_item.setParentItem(self.paper)
             treble_item.setZValue(5)
